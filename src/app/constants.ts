1053878cd3a4ec64698bde365df4870f0d446ac8
Douglas Siqueira <douglasasiqueira@hotmail.com>
2023-10-16 02:16:33 -0300

    chore: changing API URL to https

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = "http://ec2-35-172-212-169.compute-1.amazonaws.com:8080";
+export const API_URL = "https://ec2-35-172-212-169.compute-1.amazonaws.com";
 
 export const AUTH_COOKIE = "auth_token";
 
